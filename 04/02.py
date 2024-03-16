0691d918f4c72fae6ea2f9595222129adc7742f4
Ismagilov Almaz <red0412@yandex.ru>
2024-03-16 21:46:39 +0500

    02

---
+++ 04/02.py
@@ -0,0 +1,18 @@
+class Acellularia:
+    pass
+
+class Cellularia:
+    class Prokaryota:
+        pass
+
+    class Eukaryota:
+        class Unicellularia:
+            pass
+
+        class Plantae:
+            pass
+
+        class Animalia:
+            pass
+
+    pass
