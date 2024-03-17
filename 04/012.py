e16c59499147afca342c9269f11f220876c9ec09
Ismagilov Almaz <red0412@yandex.ru>
2024-03-17 15:18:06 +0500

    012

--- 04/012.py
+++ 04/012.py
@@ -22,3 +22,24 @@ class C(A, B):
 class D(A, B):
     def __str__(self):
         return B.__str__(self)
+
+
+def new_method(arg):
+    return  "new method"
+
+def new_method2(arg):
+    return "new method 2"
+
+
+A.__str__ = new_method
+B.__str__ = new_method2
+c = C()
+c.hello()
+c.good_evening()
+d = D()
+d.hello()
+d.good_evening()
+print(isinstance(c, A), isinstance(c, B))
+print(isinstance(d, A), isinstance(d, B))
+print(c)
+print(d)
